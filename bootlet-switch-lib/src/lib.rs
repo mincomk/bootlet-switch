326f66583237408f2f8ac7dfef24a9b859151aa3
minco <mail@drchi.co.kr>
2026-02-09 16:54:58 +0900

    feat: add wait-port

--- bootlet-switch-lib/src/lib.rs
+++ bootlet-switch-lib/src/lib.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use crate::comms::{send_message, wait_for_message};
 use types::{MessageBoardToPc, MessagePcToBoard};
 
@@ -13,6 +15,22 @@ pub fn get_switch_state() -> crate::Result<bool> {
     let mut port = find_device()?;
 
     ask_switch_state(&mut *port)
+}
+
+pub fn wait_port(max_secs: u32, sleep_ms: u32) -> crate::Result<()> {
+    let instant = Instant::now();
+    loop {
+        match find_device() {
+            Ok(_) => return Ok(()),
+            Err(_) => {
+                if instant.elapsed().as_secs() >= max_secs as u64 {
+                    return Err(crate::Error::DeviceNotFound);
+                }
+            }
+        }
+
+        std::thread::sleep(std::time::Duration::from_millis(sleep_ms as u64));
+    }
 }
 
 fn ask_switch_state(serial: &mut dyn serialport::SerialPort) -> crate::Result<bool> {
--- run-bootlet-switch/src/main.rs
+++ run-bootlet-switch/src/main.rs
@@ -1,6 +1,13 @@
-use std::process::{Command, Stdio};
+use std::{
+    env::args,
+    process::{Command, Stdio},
+};
 
 fn main() -> anyhow::Result<()> {
+    if args().any(|arg| arg == "--wait") {
+        bootlet_switch_lib::wait_port(5, 100)?;
+    }
+
     let state = bootlet_switch_lib::get_switch_state()?;
 
     let target = if state {
